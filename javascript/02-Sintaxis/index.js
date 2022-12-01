b75d865aa4560b92ed87b55353b845d862e6cee8
Jvilchezru <vilchez.10.2001@gmail.com>
2022-11-30 23:11:45 -0500

    Sintaxis, variables, palabras reservadas JS

---
+++ javascript/02-Sintaxis/index.js
@@ -0,0 +1,14 @@
+const list = [
+  "Jesus Vilchez",
+  21,
+  true,
+  new Date(2001, 9, 4),
+  {
+    title: "Metamorfosis",
+    author: "Ovidio",
+    date: new Date(2020, 0, 5),
+    url: "https://www.amazon.es/Metamorfosis-Cl%C3%A1sica-Ovidio/dp/8467037482",
+  },
+];
+
+console.log(list);
