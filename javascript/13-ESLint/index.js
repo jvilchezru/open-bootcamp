986173f4e0559fc8334f275eb0e6aaa2107978bf
Jvilchezru <vilchez.10.2001@gmail.com>
2022-12-15 18:14:11 -0500

    ESLint

---
+++ javascript/13-ESLint/index.js
@@ -0,0 +1,12 @@
+// Sesión 4 - Strings (cadenas de caracteres)
+
+let str_comillas_simples = 'El otro día me dijo literalmente ve a sacar la basura'
+let str_comillas_dobles = 'El otro día me dijo literalmente ve a sacar la basura'
+let str_backticks = 'Hola esto es un string con backticks'
+
+console.log(str_comillas_simples)
+console.log(str_comillas_dobles)
+console.log(str_backticks)
+
+//// Introducción de variables en html
+let libros = ['Empieza por el por qué', 'El monje que vendió su Ferrari', 'El poder del ahora']
