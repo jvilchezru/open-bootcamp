dea16c12e521710fbb7656802a52caa6f33080c3
Jvilchezru <vilchez.10.2001@gmail.com>
2022-12-17 16:48:31 -0500

    Persistencia de datos

---
+++ javascript/15-Data-persistence/index.js
@@ -0,0 +1,17 @@
+const name = "Jesus"
+const lastname = "Vilchez"
+
+const person = {
+  name,
+  lastname
+}
+
+// Session Storage
+// sessionStorage.setItem("data-session", JSON.stringify(person))
+
+// Local Storage
+// localStorage.setItem("data-local", JSON.stringify(person))
+
+// Cookie
+// document.cookie = `dataCookie=${JSON.stringify(person)};expires=${new Date(new Date().getTime() + (2 * 60000)).toUTCString()}`
+
