ed056392fdc0f98e77a2c1543e951d9317af6208
Jvilchezru <vilchez.10.2001@gmail.com>
2023-03-22 20:35:40 -0500

    java-basico

---
+++ java-basico/ejercicio-tema1/src/com/openbootcamp/Ejercicio1.java
@@ -0,0 +1,49 @@
+package com.openbootcamp;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        /*
+            1. Numéricos
+         */
+
+        // 1.1. Enteros
+        byte num1 = 5;
+        short num2 = 20;
+        int num3 = 50;
+        long num4 = 1000;
+
+        System.out.println("Enteros: " + num1 + ", " + num2 + ", " + num3 + ", " + num4);
+
+        // 1.2. Decimales
+        float dec1 = 1.2f;
+        double dec2 = 50.4d;
+
+        System.out.println("Decimales: " + dec1 + ", " + dec2);
+
+        /*
+            2. Booleanos
+         */
+
+        boolean bol1 = false;
+        boolean bol2 = true;
+
+        System.out.println("Booleanos: " + bol1 + ", " + bol2);
+
+        /*
+            3. Texto
+         */
+
+        // 3.1. Cadena de Texto
+        String name = "Jesus Vilchez";
+
+        System.out.println("Cadena de tecto: " + name);
+
+        // 3.2. Caracter
+        char letter = 'a';
+
+        System.out.println("Caracter: " + letter);
+
+    }
+}
