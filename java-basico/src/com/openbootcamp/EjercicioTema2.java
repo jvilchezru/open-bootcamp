088f355070ebf11012625d4e7ae8457d66d66574
Jvilchezru <vilchez.10.2001@gmail.com>
2023-04-18 19:40:04 -0500

    funciones-java

--- java-basico/src/com/openbootcamp/EjercicioTema2.java
+++ java-basico/src/com/openbootcamp/EjercicioTema2.java
@@ -1,5 +1,23 @@
 package com.openbootcamp;
 
+import java.util.Scanner;
+
 public class EjercicioTema2 {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double price, priceIVA;
+
+        System.out.print("> Ingrese un precio: ");
+        price = in.nextDouble();
+
+
+        priceIVA = addIVA(price);
+
+        System.out.println("\nEl precio con IVA incluido es " + priceIVA);
+    }
+
+    static double addIVA(double price) {
+        //  IVA = 18%
+        return price * 1.18;
+    }
 }
