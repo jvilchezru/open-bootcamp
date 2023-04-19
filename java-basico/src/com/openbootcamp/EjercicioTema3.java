9dab0ddbcccf70f6155e0bc22047bd8df00ebd6e
Jvilchezru <vilchez.10.2001@gmail.com>
2023-04-18 20:26:06 -0500

    estructuras-control

---
+++ java-basico/src/com/openbootcamp/EjercicioTema3.java
@@ -0,0 +1,11 @@
+package com.openbootcamp;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+        String[] words = {"Hello ", "world, ", "I'm ", "Jesus Vilchez."};
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.print(words[i]);
+        }
+    }
+}
